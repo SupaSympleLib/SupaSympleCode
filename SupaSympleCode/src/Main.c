08c86215720932a216dd4bb85c856b091bde6db4
Treidex <treidexsemextetry@gmail.com>
2021-02-19 12:56:57 -0800

    ?

--- SupaSympleCode/src/Main.c
+++ SupaSympleCode/src/Main.c
@@ -7,6 +7,8 @@ int main()
 	printf("Source of '%s':\n", file->Name);
 	SetConsoleColor(ConsoleColor_DarkCyan);
 	puts(file->Source);
+	Token *token = Lex(file);
+	printf("First Token (%p): Kind = %i, Text = %.*s", token, token->Kind, token->Length, token->Text);
 	CloseFile(file);
 	getchar();
 }
